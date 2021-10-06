1c34ce6e0750e615958feb032491a9c4da337db6
Berk Durmus <berkdurmus@yahoo.com>
2021-10-06 20:27:16 +0300

    image uploader deployment

--- scripts/radseason.sh
+++ scripts/radseason.sh
@@ -1,5 +1,6 @@
 cd /app/scrapotrip && \
 node scrapers/events/radseason.js --output-file /app/out.jsonl && \
+node scrapers/events/imageUploader.js && \
 cd /app/solodata-external-data-proccessor && \
 export EVENTS_JSON_PATH=/app/out.jsonl && \
 python3 -m solodata.tasks.import_events_with_change_detection
