44a1f90b421d89e6285d24aff69c89fa834b2b33
Berk Durmus <berkdurmus@yahoo.com>
2021-10-08 14:12:54 +0300

    Create covid-status.sh

---
+++ scripts/covid-status.sh
@@ -0,0 +1,5 @@
+cd /app/scrapotrip && \
+node scrapers/covid-status/airtable.js && \
+node scrapers/covid-status/airtable-proccessor.js && \
+node scrapers/covid-status/airtable-update-tracker.js && \
+node scrapers/covid-status/airtable-mongo-uploader.js
