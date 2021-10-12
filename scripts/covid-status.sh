0d69ac2b409fd8b08d058bbbe0518706033c800c
Berk Durmus <berkdurmus@yahoo.com>
2021-10-12 23:28:01 +0300

    kayak-restrictions crawl deployment

--- scripts/covid-status.sh
+++ scripts/covid-status.sh
@@ -1,4 +1,5 @@
 cd /app/scrapotrip && \
+node scrapers/covid-status/kayak-restrictions.js && \
 node scrapers/covid-status/airtable.js && \
 node scrapers/covid-status/airtable-proccessor.js && \
 node scrapers/covid-status/airtable-update-tracker.js && \
