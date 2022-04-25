59b803da16d02add19bfe5e9865b0bbd8af96c67
Berk Durmus <berkdurmus@yahoo.com>
2022-04-26 01:53:36 +0300

    Running update-safety inside covid-status script

--- scripts/covid-status.sh
+++ scripts/covid-status.sh
@@ -1,4 +1,5 @@
 cd /app/scrapotrip && \
-node scripts/update-covid-restrictions.js
+node scripts/update-covid-restrictions.js && \
+node scripts/update-safety.js
 [[ -z $NO_NOTIFICATION ]] && cd /app/solotrip-backend && \
 node scripts/update-recommendations.js
